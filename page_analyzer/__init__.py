4882384d355c5852b1ffadef1cc0d8d70ec39114
Dmitriy <kerokero@yandex.ru>
2025-10-07 15:17:45 +0300

    move route registration to init_routes function

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from os import getenv
+from dotenv import load_dotenv
+from .routes import init_routes
+
+
 app = Flask(
     __name__,
     template_folder="../templates",
     static_folder="../static"
 )
-from . import routes
 
+load_dotenv()
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
+init_routes(app)
--- page_analyzer/routes.py
+++ page_analyzer/routes.py
@@ -1,16 +1,9 @@
-from flask import (
-    render_template,
-)
-from dotenv import load_dotenv
-from os import getenv
-from page_analyzer import app
-
-load_dotenv()
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+from flask import render_template
 
 
-@app.get('/')
-def index():
-    return render_template(
-        'index.html'
-    )
+def init_routes(app):
+    @app.get('/')
+    def index():
+        return render_template(
+            'index.html'
+        )
