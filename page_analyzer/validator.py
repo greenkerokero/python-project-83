98ccfbeda869b872d83af613441d410fabe6e94d
Dmitriy <kerokero@yandex.ru>
2025-10-10 21:38:24 +0300

    fix validator module

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -3,6 +3,6 @@ from validators import url
 
 def validate(url_string):
     errors = {}
-    if not url(url_string):
+    if not url(url_string) or len(url_string) > 255:
         errors['url'] = 'Некорректный URL'
     return errors
