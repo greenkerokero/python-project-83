b5ad089aa9d2d76444f1cd50d531c8b385673dc9
Dmitriy <kerokero@yandex.ru>
2025-10-11 11:15:22 +0300

    remove debug code

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -3,6 +3,8 @@ from validators import url
 
 def validate(url_string):
     errors = {}
+    print(not url(url_string))
+    print(len(url_string) > 255)
     if not url(url_string) or len(url_string) > 255:
         errors['url'] = 'Некорректный URL'
     return errors
