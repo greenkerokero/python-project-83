72d94331b3f40c2e75640ffcc1cd233e8ca0762d
Dmitriy <kerokero@yandex.ru>
2025-10-13 21:54:37 +0300

    Fix buld.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-#curl -LsSf https://astral.sh/uv/install.sh | sh
-#source $HOME/.local/bin/env
-#make install && psql -a -d "$DATABASE_URL" -f database.sql
-
-
-set -o errexit  # аварийно выйти при ошибке
-set -o nounset  # ошибка, если переменная не определена
-set -o pipefail # если команда в пайпе упала — тоже ошибка
-
-echo "==== Render build environment (safe view) ===="
-env | grep -E 'RENDER|DATABASE|PORT|PYTHON' || true
-echo "============================================="
-
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
-
-echo "Running migrations..."
-psql -a -d "$DATABASE_URL" -f database.sql
+make install && psql -a -d "$DATABASE_URL" -f database.sql
