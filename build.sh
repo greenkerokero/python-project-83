e694add63c529d5e93d3440c844111889f620ce1
Dmitriy <kerokero@yandex.ru>
2025-10-13 19:54:38 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install && psql -a -d name -f database.sql
