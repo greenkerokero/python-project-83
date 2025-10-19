203809c99d609e2928fa69dc85426e579985d09a
Dmitriy <kerokero@yandex.ru>
2025-10-19 16:21:15 +0300

    Add URL check routes and integrate UrlCheckRepository

--- page_analyzer/routes.py
+++ page_analyzer/routes.py
@@ -8,11 +8,12 @@ from flask import (
 )
 from urllib.parse import urlparse
 from page_analyzer.validator import validate
-from page_analyzer.repository import UrlRepository
+from page_analyzer.repository import UrlRepository, UrlCheckRepository
 
 
 def init_routes(app):
-    repo = UrlRepository(app.config['DATABASE_URL'])
+    url_repo = UrlRepository(app.config['DATABASE_URL'])
+    url_check_repo = UrlCheckRepository(app.config['DATABASE_URL'])
 
     @app.get('/')
     def index():
@@ -22,19 +23,36 @@ def init_routes(app):
 
     @app.get('/urls')
     def urls_get():
-        urls = repo.get_all()
+        urls = url_repo.get_all()
+        sites = []
+        for url in urls:
+            last_check = url_check_repo.get_last_check(url.get('id'))
+            if last_check:
+                last_check = last_check.strftime('%Y-%m-%d')
+            else:
+                last_check = ''
+
+            site = {
+                'id': url['id'],
+                'site': url['url'],
+                'last_check': last_check,
+                'code': '',
+            }
+            sites.append(site)
         return render_template(
             'urls/index.html',
-            urls=urls,
+            urls=sites,
         )
 
     @app.get('/urls/<id>')
     def urls_show(id):
         messages = get_flashed_messages(with_categories=True)
-        url_data = repo.find(id)
+        url = url_repo.find(id)
+        checks = url_check_repo.find_by_url_id(id)
         return render_template(
             'urls/show.html',
-            url=url_data,
+            url=url,
+            checks=checks,
             messages=messages,
         )
 
@@ -49,18 +67,27 @@ def init_routes(app):
             messages = get_flashed_messages(with_categories=True)
             return render_template(
                 'index.html',
-                messages=messages,
                 url=url,
+                messages=messages,
             )
 
         url = urlparse(url).netloc
         raw_url['url'] = url
 
-        existing_url = repo.find_by_url(url)
+        existing_url = url_repo.find_by_url(url)
         if existing_url:
             flash('Страница уже существует', 'info')
             return redirect(url_for('urls_show', id=existing_url['id']))
 
-        id = repo.save(raw_url)
+        id = url_repo.save(raw_url)
         flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('urls_show', id=id))
+
+    @app.post('/urls/<id>/checks')
+    def urls_check(id):
+        check = {
+            'url_id': id,
+        }
+        url_check_repo.save(check)
+        flash('Страница успешно проверена', 'success')
         return redirect(url_for('urls_show', id=id))
