e59ddf29db3a515d7969c3dfb0018b4e25f146d0
Dmitriy <kerokero@yandex.ru>
2025-11-04 19:37:42 +0300

    Convert to use blueprint

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from os import getenv
 from dotenv import load_dotenv
-from page_analyzer.routes import init_routes
-
-
-app = Flask(
-    __name__,
-    template_folder="../templates",
-    static_folder="../static"
-)
+from page_analyzer.repository import UrlRepository, UrlCheckRepository
 
 load_dotenv()
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-app.config['DATABASE_URL'] = getenv('DATABASE_URL')
 
-init_routes(app)
+
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder="../templates",
+        static_folder="../static"
+    )
+
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+    app.config['DATABASE_URL'] = getenv('DATABASE_URL')
+
+    app.url_repo = UrlRepository(app.config['DATABASE_URL'])
+    app.url_check_repo = UrlCheckRepository(app.config['DATABASE_URL'])
+
+    from page_analyzer.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from page_analyzer.urls import bp as urls_bp
+    app.register_blueprint(urls_bp)
+
+    return app
+
+
+app = create_app()
---
+++ page_analyzer/urls.py
@@ -0,0 +1,122 @@
+from flask import (
+    get_flashed_messages,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for,
+    Blueprint,
+    current_app
+)
+from page_analyzer.services import validate, get_site_name
+from page_analyzer.checker import check
+
+
+bp = Blueprint('urls', __name__)
+
+ALERT_CLASSES = {
+    'success': 'success',
+    'info': 'info',
+    'danger': 'danger'
+}
+
+
+@bp.get('/urls')
+def urls_get():
+    url_repo = current_app.url_repo
+    url_check_repo = current_app.url_check_repo
+
+    urls = url_repo.get_all()
+    sites = []
+    for url in urls:
+        last_check_time = url_check_repo.get_last_check_time(url.get('id'))
+        if last_check_time:
+            last_check_time = last_check_time.strftime('%Y-%m-%d')
+        else:
+            last_check_time = ''
+
+        last_check_code = url_check_repo.get_last_check_code(url.get('id'))
+        if not last_check_code:
+            last_check_code = ''
+
+        site = {
+            'id': url['id'],
+            'site': url['url'],
+            'last_check': last_check_time,
+            'code': last_check_code,
+        }
+        sites.append(site)
+    return render_template(
+        'urls/index.html',
+        urls=sites,
+    )
+
+
+@bp.get('/urls/<id>')
+def urls_show(id):
+    url_repo = current_app.url_repo
+    url_check_repo = current_app.url_check_repo
+
+    messages = get_flashed_messages(with_categories=True)
+    url = url_repo.find(id)
+    checks = url_check_repo.find_by_url_id(id)
+
+    return render_template(
+        'urls/show.html',
+        url=url,
+        checks=checks,
+        messages=messages,
+        alert_classes=ALERT_CLASSES,
+    )
+
+
+@bp.post('/urls')
+def urls_post():
+    url_repo = current_app.url_repo
+
+    form_data = request.form.to_dict()
+    url = get_site_name(form_data['url'])
+
+    errors = validate(url)
+    if errors:
+        flash('Некорректный URL', 'danger')
+        messages = get_flashed_messages(with_categories=True)
+        return render_template(
+            'index.html',
+            url=url,
+            messages=messages,
+            alert_classes=ALERT_CLASSES,
+        ), 422
+
+    existing_url = url_repo.find_by_url(url)
+    if existing_url:
+        flash('Страница уже существует', 'info')
+        return redirect(url_for('urls.urls_show', id=existing_url['id']))
+
+    form_data['url'] = url
+    url_id = url_repo.save(form_data)
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('urls.urls_show', id=url_id))
+
+
+@bp.post('/urls/<id>/checks')
+def urls_check(id):
+    url_repo = current_app.url_repo
+    url_check_repo = current_app.url_check_repo
+
+    saved_url = url_repo.find(id).get('url')
+
+    check_result = check(saved_url)
+    if 'error' in check_result:
+        flash(
+            'Произошла ошибка при проверке', 'danger'
+        )
+    else:
+        check_data = {
+            'url_id': id,
+        }
+        check_data.update(check_result)
+        url_check_repo.save(check_data)
+        flash('Страница успешно проверена', 'success')
+
+    return redirect(url_for('urls.urls_show', id=id))
