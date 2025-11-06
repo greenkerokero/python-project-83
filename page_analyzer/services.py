ea79ef59972b3dba0ffc64ea193f3e32b23851e7
Dmitriy <kerokero@yandex.ru>
2025-11-06 17:42:41 +0300

    Fix get_site_name function

--- page_analyzer/services.py
+++ page_analyzer/services.py
@@ -8,12 +8,12 @@ def get_site_name(url_string):
     if parce_url.scheme:
         return f'{parce_url.scheme}://{parce_url.netloc}'
     else:
-        return f'https://{parce_url.netloc}'
+        return f'https://{parce_url.path}'
 
 
 def validate(url_string):
-    norm_url = get_site_name(url_string)
     errors = {}
-    if not url(norm_url) or len(norm_url) > 255:
+
+    if not url(url_string) or len(url_string) > 255:
         errors['url'] = 'Некорректный URL'
     return errors
