b4a0071a7618f14221003cf668cd0db837aec5c5
Dmitriy <kerokero@yandex.ru>
2025-11-12 14:54:27 +0300

    Add loggin handler in package

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
-from os import getenv
+from os import getenv, path, mkdir, getpid
 from dotenv import load_dotenv
 from page_analyzer.repository import UrlRepository, UrlCheckRepository
+from logging import Formatter, INFO
+from logging.handlers import TimedRotatingFileHandler
+from uuid import uuid4
 
 load_dotenv()
 
 
 def create_app():
+    LOGS_DIR = 'logs'
+    worker_id = getpid()
+    operation_id = str(uuid4())
+
     app = Flask(
         __name__,
         template_folder='templates',
@@ -27,6 +34,23 @@ def create_app():
 
     from page_analyzer.urls import bp as urls_bp
     app.register_blueprint(urls_bp)
+
+    if not path.exists(LOGS_DIR):
+        mkdir(LOGS_DIR)
+    file_handler = TimedRotatingFileHandler(
+        filename=f'{LOGS_DIR}/app.log',
+        when='W0',
+        interval=1,
+        backupCount=4,
+        encoding='utf-8'
+    )
+    file_handler.setFormatter(Formatter(
+        f'%(asctime)s %(levelname)s: [Worker ID: {worker_id}] %(message)s'))
+    file_handler.setLevel(INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(INFO)
+    app.logger.info(f'[Operation ID: {operation_id}] Page analyzer startup')
 
     return app
 
